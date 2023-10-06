1d2e74d04260dfe9b25400061c079ff1971813cd
gustavo.andrade <gustavo.andrade@tray.net.br>
2023-10-06 09:49:41 -0300

    att

--- components/pages/Home/Presentation/index.js
+++ components/pages/Home/Presentation/index.js
@@ -4,12 +4,69 @@ function Presentation() {
   return (
     <h1 className={styles.presentation}>
       <div className="margin-page display-flex">
-        {['U', 'X'].map((letter, index) => <div key={index}> { letter === 'space' ? <div style={{width: '4vw'}} /> : <p className="enterprise-name" style={{animationDelay: `${index}00ms`, color: "var(--primary-color)"}}>{letter}</p> }</div> )}
-        {['space', '&', 'space'].map((letter, index) => <div key={index}> { letter === 'space' ? <div style={{width: '4vw'}} /> : <p className="enterprise-name" style={{animationDelay: `${index}00ms`}}>{letter}</p> }</div> )}
-        {['U', 'I'].map((letter, index) => <div key={index}> { letter === 'space' ? <div style={{width: '4vw'}} /> : <p className="enterprise-name" style={{animationDelay: `${index}00ms`, color: "var(--primary-color)"}}>{letter}</p> }</div> )}
+        {['G', 'U', 'S', 'T', 'A', 'V', 'O'].map((letter, index) =>
+          <div key={index}>
+            <p className="enterprise-name" style={{animationDelay: `${index}00ms`}}>
+              {letter}
+            </p>
+          </div>
+        )}
       </div>
       <div className="margin-page display-flex">
-        {['D', 'E', 'S', 'I', 'G', 'N', 'E', 'R'].map((letter, index) => <div key={index}> { letter === 'space' ? <div style={{width: '4vw'}} /> : <p className="enterprise-name" style={{animationDelay: `${index}00ms`}}>{letter}</p> }</div> )}
+        {['A', 'N', 'D', 'R', 'A', 'D', 'E'].map((letter, index) =>
+          <div key={index}>
+            <p className="enterprise-name" style={{animationDelay: `${index}00ms`}}>
+              {letter}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="margin-page display-flex">
+        {['U', 'X'].map((letter, index) =>
+          <div key={index}>
+            <p className="enterprise-name enterprise-name-sm" style={{animationDelay: `${index}00ms`, color: "var(--primary-color)"}}>
+              {letter}
+            </p>
+          </div>
+        )}
+
+        {['space', '&', 'space'].map((letter, index) =>
+          <div key={index}>
+            {
+              letter === 'space'
+              ?
+              <div style={{width: '2vw'}} />
+              :
+              <p className="enterprise-name enterprise-name-sm" style={{animationDelay: `${index}00ms`}}>
+                {letter}
+              </p>
+            }
+          </div>
+        )}
+
+        {['U', 'I'].map((letter, index) =>
+          <div key={index}>
+            <p className="enterprise-name enterprise-name-sm" style={{animationDelay: `${index}00ms`, color: "var(--primary-color)"}}>
+              {letter}
+            </p>
+          </div>
+        )}
+
+        {['space', 'D', 'E', 'S', 'I', 'G', 'N', 'E', 'R'].map((letter, index) =>
+          <div key={index}>
+            {
+              letter === 'space'
+              ?
+              <div style={{width: '2vw'}} />
+              :
+              <p className="enterprise-name enterprise-name-sm" style={{animationDelay: `${index}00ms`}}>
+                {letter}
+              </p>
+            }
+          </div>
+        )}
+      </div>
+      <div className="margin-page display-flex">
       </div>
     </h1>
   )
