45036c7f29be8102e70675481c3c89cb95b40262
gustavo.andrade <gustavo.andrade@tray.net.br>
2025-03-10 17:00:06 -0300

    lifeguard

--- components/Project/LifeGuard/index.js
+++ components/Project/LifeGuard/index.js
@@ -7,8 +7,14 @@ function LifeGuard() {
     <div className="width-full display-flex flex-direction-column margin-product-page">
       <h1>Solução de interface para aplicativo de cameras de segurança eletrônica</h1>
       <img className={styles.capaProduct} src="/img/lifeguard/banner.png" />
-      <h1>Desenvolvimento da solução</h1>
-      <p className={styles.paragraph}>Em breve.</p>
+      <p className={styles.paragraph}>Esse foi o layout sugerido para empresa life tecnologia para seu aplciativo "LifeGuard", de cameras de segurança.</p>
+      <p className={styles.paragraph}>Nele era possível ver a lsitagem de cameras, ver uma camera em tempo real e fazer a gestão ce acesso dos usuários da conta.</p>
+      <h1>Fluxo de autenticação</h1>
+      <img className={styles.capaProduct} src="/img/lifeguard/login.png" />
+      <h1>Visão das cameras</h1>
+      <img className={styles.capaProduct} src="/img/lifeguard/cameras.png" />
+      <h1>Gestão de usuários</h1>
+      <img className={styles.capaProduct} src="/img/lifeguard/configuracoes.png" />
     </div>
   );
 }
